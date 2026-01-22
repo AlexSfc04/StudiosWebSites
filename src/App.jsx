bea41753bad11be5cccee36b25812e5918a6fb4a
AlexSfc04 <amoricoalejandro@alumnos.cei.es>
2026-01-22 12:10:53 +0100

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout/Layout.jsx";
-import HomePage from "./pages/HomePage.jsx";
-import ServicesPage from "./pages/ServicesPage.jsx";
-// ...importa el resto
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import HomePage from './pages/HomePage'
+import ServicesPage from './pages/ServicesPage'
+import SectorsPage from './pages/SectorsPage'
+import PortfolioPage from './pages/PortfolioPage'
+import BlogPage from './pages/BlogPage'
+import ContactPage from './pages/ContactPage'
 
 function App() {
   return (
@@ -12,11 +15,11 @@ function App() {
         <Route path="/servicios" element={<ServicesPage />} />
         <Route path="/sectores" element={<SectorsPage />} />
         <Route path="/portfolio" element={<PortfolioPage />} />
-        <Route path="/blog" element={<AboutPage />} />
+        <Route path="/blog" element={<BlogPage />} />
         <Route path="/contacto" element={<ContactPage />} />
       </Routes>
     </Layout>
-  );
+  )
 }
 
-export default App;
+export default App
