252fb2679afc9749d1f044655f469d5362731543
AlexSfc04 <amoricoalejandro@alumnos.cei.es>
2026-01-22 11:46:21 +0100

    add pages

---
+++ src/HomePage.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Home/Hero.jsx";
+import ServicesSummary from "../components/Home/ServicesSummary.jsx";
+
+function HomePage() {
+
+    return (
+        <>
+        <Hero />
+        <ServicesSummary />
+        {/* añade más secciones según tu diseño */}
+        </>
+    );
+}
+
+export default HomePage;
