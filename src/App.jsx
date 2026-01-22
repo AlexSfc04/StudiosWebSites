1e163668a6071ab9004f1307d01bf700d3cab8cb
AlexSfc04 <amoricoalejandro@alumnos.cei.es>
2026-01-22 11:53:44 +0100

    add routes pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout/Layout.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import ServicesPage from "./pages/ServicesPage.jsx";
+// ...importa el resto
+
 function App() {
-  return <h1>StudiosWebSites</h1>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/servicios" element={<ServicesPage />} />
+        <Route path="/sectores" element={<SectorsPage />} />
+        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route path="/blog" element={<AboutPage />} />
+        <Route path="/contacto" element={<ContactPage />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
