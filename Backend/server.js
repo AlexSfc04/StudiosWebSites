d8fa07d3d99f6ca959d4b9f6837bcb4136210b72
AlexSfc04 <amoricoalejandro@alumnos.cei.es>
2026-05-12 10:51:57 +0200

    configuration profile image

--- Backend/server.js
+++ Backend/server.js
@@ -31,8 +31,8 @@ app.use(cors({
   },
   credentials: true,
 }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '5mb' }))
+app.use(express.urlencoded({ extended: true, limit: '5mb' }))
 
 // Rutas
 app.use('/auth', authRoutes)
